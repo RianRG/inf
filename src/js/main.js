04358448fb3c0fe68940c610d74381be45a0cfbd
RianRG <rianrgomes@gmail.com>
2024-11-21 22:12:03 -0300

    feat: improve styles, add box "Por que escolher informática?"

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,27 @@ console.log('Inf-101')
 
 const menu = document.querySelector('.hamburger');
 const ul = document.querySelector('ul')
+const h1 = document.querySelector('#home h1');
+
 menu.addEventListener('click', () =>{
-  ul.classList.toggle('active')
-  menu.classList.toggle('active')
+  ul.classList.toggle('active');
+  menu.classList.toggle('active');
 })
 
 const removeMenuClass = () =>{
   ul.classList.remove('active');
-  menu.classList.remove('active')
+  menu.classList.remove('active');
 }
+
+const macchine = (text) =>{
+  const letters = text.innerHTML.split('');
+  text.innerHTML='';
+
+  letters.forEach((cont, k) =>{
+    setTimeout(letter =>{
+      h1.innerHTML+=cont;
+    }, 120*k);
+  })
+}
+
+macchine(h1);
